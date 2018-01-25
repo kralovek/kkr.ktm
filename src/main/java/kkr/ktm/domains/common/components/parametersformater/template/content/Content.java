2f1cc0e30502354f2d302edc900b96f1037711b2
Kare Kralovec <karel.kralovec@gmail.com>
2018-01-25 15:13:18 +0100

    Values

--- src/main/java/kkr/ktm/domains/common/components/parametersformater/template/content/Content.java
+++ src/main/java/kkr/ktm/domains/common/components/parametersformater/template/content/Content.java
@@ -3,16 +3,11 @@ package kkr.ktm.domains.common.components.parametersformater.template.content;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Content
- *
- * @author KRALOVEC-99999
- */
 public class Content {
 
-    private List<Object> contents = new ArrayList<Object>();
+	private List<Object> contents = new ArrayList<Object>();
 
-    public List<Object> getContents() {
-        return contents;
-    }
+	public List<Object> getContents() {
+		return contents;
+	}
 }
