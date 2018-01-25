ddd4341aaa0860d39fc41eec137d76f6c688fd56
Kare Kralovec <karel.kralovec@gmail.com>
2018-01-25 15:13:42 +0100

    Values

--- src/main/java/kkr/ktm/domains/common/components/parametersformater/template/tags/Attribute.java
+++ src/main/java/kkr/ktm/domains/common/components/parametersformater/template/tags/Attribute.java
@@ -1,27 +1,22 @@
 package kkr.ktm.domains.common.components.parametersformater.template.tags;
 
-/**
- * Attribute
- *
- * @author KRALOVEC-99999
- */
 public class Attribute {
-    private String name;
-    private String value;
+	private String name;
+	private String value;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(final String pName) {
-        this.name = pName;
-    }
+	public void setName(final String pName) {
+		this.name = pName;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(final String pValue) {
-        this.value = pValue;
-    }
+	public void setValue(final String pValue) {
+		this.value = pValue;
+	}
 }
